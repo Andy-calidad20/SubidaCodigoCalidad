02340f8e972425ac3ce19b5ffe4720f165ac3c71
Andy-calidad20 <75270471+Andy-calidad20@users.noreply.github.com>
2020-12-01 21:23:15 -0600

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,17 +1,16 @@
 <?php
-
-class Conexion{
-
-	static public function conectar(){
-
-		$link = new PDO("mysql:host=localhost;dbname=sis_inventario",
-			            "root",
-			            "");
-
-		$link->exec("set names utf8");
-
-		return $link;
-
-	}
-
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sisventasdbserver.database.windows.net,1433; Database = Inventario", "SISventasServer", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "SISventasServer", "pwd" => "{your_password_here}", "Database" => "Inventario", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:sisventasdbserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
