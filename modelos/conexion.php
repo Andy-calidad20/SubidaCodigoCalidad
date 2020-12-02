5bfbe5e113d2819d5923a59d8a589000ad704f41
Andy-calidad20 <75270471+Andy-calidad20@users.noreply.github.com>
2020-12-01 21:44:44 -0600

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,19 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
-try {
     $con=mysqli_init(); 
     mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
     mysqli_real_connect($con, "serv-mysql-prueba.mysql.database.azure.com", "prueba@serv-mysql-prueba", {your_password}, {your_database}, 3306);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "SISventasServer", "pwd" => "{your_password_here}", "Database" => "Inventario", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:sisventasdbserver.database.windows.net,1433";
-con=mysqli_init(); 
-    mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-    mysqli_real_connect($con, "serv-mysql-prueba.mysql.database.azure.com", "prueba@serv-mysql-prueba", {your_password}, {your_database}, 3306);
 ?>
